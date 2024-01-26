856cae635687a3c1178f806cbe18882c0fdbd62b
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-01-26 06:00:08 -0500

    adding pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Features from './components/Features'
 import About from './components/About'
+import Pricing from './components/Pricing'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Home/>
       <Features/>
       <About/>
+      <Pricing/>
     </>
   )
 }
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Pricing() {
+  return (
+    <div className="md:px-14 p-4 max-w-s mx-auto py-10 ">
+      <div className="text-center">
+        <h2 className="font-extrabold md:text-5xl text-primary mb-2" text-3xl>
+          Here are all our plans
+        </h2>
+        <p className="text-tartiary md:w-1/3 mx-auto px-4 ">
+          A simple paragraph is comprised of three major components. The which
+          is often a declarative sentence.
+        </p>
+      </div>
+    </div>
+  );
+}
